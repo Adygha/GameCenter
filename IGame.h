f55c11fde87386044b5c25325cfeb9340aa0404b
Janty Azmat <janty.azmat@gmail.com>
2024-01-31 18:08:07 +0100

    fix: Add virtual destructor to IGame
    Add virtual destructor to IGame to properly delete using IGame pointer

--- IGame.h
+++ IGame.h
@@ -6,8 +6,9 @@
 
 class IGame {
 public:
-	virtual int play(char* bet, int amount) = 0;	// Place amount money on position bet and return winnings
-	virtual int getID() const = 0;					// Returns a unique ID for the actual game type
+	virtual int play(char* bet, int amount) = 0; // Place amount money on position bet and return winnings
+	virtual int getID() const = 0; // Returns a unique ID for the actual game type
+	virtual ~IGame(){} // To properly delete using IGame pointer
 };
 
 
